8cca28cea783ba874a9e03e28017efa0f6f5d746
unknown <mateus.gcr.tog@hotmail.com>
2021-01-05 22:09:22 -0300

    GET /products

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,7 +25,7 @@ INSERT INTO tb_order_product (order_id, product_id) VALUES (3 , 4);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (4 , 2);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (4 , 6);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (5 , 4);
-INSERT INTO tb_order_product (order_id, product_id) VALUES (5 , 6);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (5 , 6); 
 INSERT INTO tb_order_product (order_id, product_id) VALUES (6 , 5);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (6 , 1);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (7 , 7);
